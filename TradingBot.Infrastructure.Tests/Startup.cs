2155b9c9f1cb062d7a083774aaa8b4b4eb6d9d8b
Lev4and <54065747+Lev4and@users.noreply.github.com>
2023-04-22 13:55:50 +0500

    Created project TradingBot.Infrastructure.Tests.

---
+++ TradingBot.Infrastructure.Tests/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Bybit.Net.Clients;
+using Bybit.Net.Objects;
+using CryptoExchange.Net.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TradingBot.Infrastructure.Tests
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            BybitClient.SetDefaultOptions(new BybitClientOptions
+            {
+                ApiCredentials = new ApiCredentials("5WtwNTNXdIAJvwOa8x", "nKHRZBUqkPEFpVh296Ldr1TSgsv2kU3RGxdU"),
+                LogLevel = LogLevel.Trace,
+            });
+
+            services.AddInfrastructure();
+        }
+    }
+}
