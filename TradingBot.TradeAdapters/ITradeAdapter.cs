ff16564de7e09bc148bdf70e2aa67b14a441bb24
Lev4and <andrey.levchenko.2001@gmail.com>
2023-06-22 21:54:43 +0500

    Removed unsed files and projects.

--- TradingBot.TradeAdapters/ITradeAdapter.cs
+++ TradingBot.TradeAdapters/ITradeAdapter.cs
@@ -9,17 +9,14 @@ namespace TradingBot.TradeAdapters
 
         Task<IEnumerable<Instrument>> GetInstrumentsAsync();
 
-        Task<IEnumerable<IQuote>> GetHistoricalQuotesAsync(Symbol symbol, Interval interval, 
-            DateTime from, DateTime to);
+        Task<IEnumerable<IQuote>> GetHistoricalQuotesAsync(Symbol symbol, Interval interval, DateTime from, DateTime to);
 
         Task<long> GetPingAsync();
 
-        Task SubscribeToPingChangedAsync(Action<long> onPingChangedHandler, 
-            CancellationToken token = default);
+        Task SubscribeToPingChangedAsync(Action<long> onPingChangedHandler, CancellationToken token = default);
 
         Task<DateTime> GetServerTimeAsync();
 
-        Task SubscribeToServerTimeChangedAsync(Action<DateTime> onServerTimeChangedHandler, 
-            CancellationToken token = default);
+        Task SubscribeToServerTimeChangedAsync(Action<DateTime> onServerTimeChangedHandler, CancellationToken token = default);
     }
 }
